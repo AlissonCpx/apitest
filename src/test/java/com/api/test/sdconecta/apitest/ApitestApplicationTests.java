3a5e9844293f1a5896eab81bb26b4e82f87ab284
AlissonCpx <alissondeandrade1@gmail.com>
2022-06-28 21:16:19 -0300

    Initial commit

---
+++ src/test/java/com/api/test/sdconecta/apitest/ApitestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.test.sdconecta.apitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
